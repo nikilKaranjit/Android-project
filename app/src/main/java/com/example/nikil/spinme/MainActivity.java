9651aebcaa78da339e7f57724e71a23c82f2942b
nikilsss <karanjitnikil@gmail.com>
2016-08-14 08:03:05 +0545

    second commit

--- app/src/main/java/com/example/nikil/spinme/MainActivity.java
+++ app/src/main/java/com/example/nikil/spinme/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
+//still need to add more
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner ;
