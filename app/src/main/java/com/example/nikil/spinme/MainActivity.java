4c150c3c075b384d3c16aafec3581cb3ecada427
nikilsss <karanjitnikil@gmail.com>
2016-08-14 07:57:38 +0545

    android spinner

---
+++ app/src/main/java/com/example/nikil/spinme/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.nikil.spinme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner ;
+    ArrayAdapter<CharSequence> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner=(Spinner) findViewById(R.id.spinner)
+    }
+
+
+}
